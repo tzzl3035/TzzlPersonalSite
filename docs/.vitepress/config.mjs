76cf9c5ddffcd937bc66878249151c24a01f5307
tzzl3035 <tzzl3035@163.com>
2024-12-10 22:14:52 +0800

    24-12-10 update title and favicon:

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "TzzlStudio Site",
+  title: "tzzl.site",
   description: "TzzlStudio Site",
   markdown: {
     math: true
   },
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+    logo: '/favicon.png',
     socialLinks: [
       { icon: 'github', link: 'https://github.com/tzzl3035/TzzlPersonalSite' }
     ]
